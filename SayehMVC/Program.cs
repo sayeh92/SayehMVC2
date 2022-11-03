0b595209650ca228e959906848b32a194a7ba3c0
Sayeh Ghaderi <sayeh.ghaderi9@gmail.com>
2022-11-03 15:23:10 +0100

    Add project files.

---
+++ SayehMVC/Program.cs
@@ -0,0 +1,66 @@
+namespace SayehMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var builder = WebApplication.CreateBuilder(args);
+            //var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            //app.Run();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddMvc();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+           // app.UseDefaultFiles();// css i wwwroot
+            app.UseStaticFiles();// html i wwwroot
+
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints act like a if/else chain, first one that is true is the one to be used!
+                endpoints.MapControllerRoute(
+                    name: "PrefLangCustomRoute",
+                    pattern: "PreferdLanguage",
+                    defaults: new { controller = "Demo", action = "PreferdLanguageCookie" }
+                    );
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+
+            app.Run();
+        }
+    }
+}
