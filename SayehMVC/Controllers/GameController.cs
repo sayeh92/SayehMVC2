67077cf2814ad8bc467843687ec774f3cc92b96f
Sayeh Ghaderi <sayeh.ghaderi9@gmail.com>
2022-11-07 00:14:01 +0100

    Maybe Last

--- SayehMVC/Controllers/GameController.cs
+++ SayehMVC/Controllers/GameController.cs
@@ -5,20 +5,32 @@ namespace SayehMVC.Controllers
 {
     public class GameController : Controller
     {
-
+        //Creating the
+        //Random Number
+        Random randomNumber = new Random();
 
         [HttpGet]
 
         public IActionResult GuessView()
         {
-            if ((string.IsNullOrEmpty(HttpContext.Session.GetString("intRnd"))))
-            {
-                int getRnd=
+            //if ((string.IsNullOrEmpty(HttpContext.Session.GetString("intRnd"))))
+            //{
+            //    int getRnd=
 
-            }
-            else
+            //}
+            //else
+            //{
+            //    ViewBag.Rnd = HttpContext.Session.GetInt32("intRnd");
+            //}
+
+            //reading the cookie
+            string? RandomNumCookie = Request.Cookies["randomNumber"];
+            if ( RandomNumCookie != null)
             {
-                ViewBag.Rnd = HttpContext.Session.GetInt32("intRnd");
+                CookieOptions mycookie = new CookieOptions();
+                mycookie.Expires = DateTime.Now.AddMinutes(-1);
+                randomNumber.Next(0, 100);
+                Response.Cookies.Append("randomNumber", "", mycookie);
             }
             return View();
 
@@ -28,17 +40,45 @@ namespace SayehMVC.Controllers
         [HttpPost]
         public IActionResult GuessView(int GuessByUser)
         {
-            if (!(string.IsNullOrEmpty(HttpContext.Session.GetString("intRnd"))))
+            //if (!(string.IsNullOrEmpty(HttpContext.Session.GetString("intRnd"))))
+            //{
+            //    string? StoredRnd = HttpContext.Session.GetString("intRnd");
+            //    string response = Models.GameLogic.GuessTheNumber(Convert.ToInt32( GuessByUser));
+            //    ViewBag.Msg = response;
+
+            //} 
+            //else
+            //{
+            //    ViewBag.Msg = "Guess your number";
+            //}
+
+            //reading the cookie
+            string? RandomNumCookie = Request.Cookies["randomNumber"];
+            int Rnd;
+
+            //Checking whether the cookie is set or not
+            if (RandomNumCookie == null)
             {
-                string? StoredRnd = HttpContext.Session.GetString("intRnd");
-                string response = Models.GameLogic.GuessTheNumber(Convert.ToInt32( GuessByUser));
-                ViewBag.Msg = response;
-               
-            } 
-            else
-            {
-                ViewBag.Msg = "Guess your number";
+                //seting up the  cookie with CookieOptions
+                CookieOptions mycookie = new CookieOptions();
+                //expiration time
+                mycookie.Expires = DateTime.Now.AddMinutes(3);
+                //min and max for random number
+                int num = randomNumber.Next(0,100);
+                //adding a new cookie
+                Response.Cookies.Append("randomNumber", num.ToString(),mycookie);
+                //assigning num to Rnd
+                Rnd = num;
             }
+            else 
+            {
+                //Conversion the cookie from string to int
+                
+               Rnd = int.Parse(RandomNumCookie);
+                
+            }
+            
+            ViewBag.Number = Models.GameLogic.GuessTheNumber(GuessByUser, Rnd);
             return View();
          
         }
--- SayehMVC/Models/GameLogic.cs
+++ SayehMVC/Models/GameLogic.cs
@@ -5,34 +5,45 @@ namespace SayehMVC.Models
 {
     public class GameLogic : GameController
     {
-        public static string GuessTheNumber(int GuessByUser, int StoredNum)
+        public static string GuessTheNumber(int GuessByUser, int Rnd)
         {
 
-            if (GuessByUser > StoredNum)
+           while( GuessByUser == Rnd)
             {
-                return "Your Number is too high";
+
+             
+                if (GuessByUser > Rnd)
+                {
+                    return "Your Number is too high";
+                }
+                if (GuessByUser < Rnd)
+                {
+                    return "Your Number is too low";
+                }
+                else
+                {
+                    return "Yout got it";
+                }
+
+                
+
             }
-            if (GuessByUser < StoredNum)
-            {
-                return "Your Number is too low";
-            }
-            else 
-            {
-                return "Yout got it";
-            }
+
+            return "...";
+
         }
 
-        public static int RandomNumber()
-        {
-            int MyNumberNum;
-            Random random = new Random();
-            MyNumberNum = random.Next(1,100);
-            return MyNumberNum;
-        }
+        //public static int RandomNumber()
+        //{
+        //    int MyNumberNum;
+        //    Random random = new Random();
+        //    MyNumberNum = random.Next(1,100);
+        //    return MyNumberNum;
+        //}
 
-        internal static string GuessTheNumber(int v)
-        {
-            throw new NotImplementedException();
-        }
+        //internal static string GuessTheNumber(int v)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
